96d488da985abec1e5a9b41574fca6154887852e
Craig Citro Default <craig+default@citro.net>
2022-08-03 17:38:40 -0500

    Fix conditional apply

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install -y ripgrep neovim zsh
 
-if [ -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
