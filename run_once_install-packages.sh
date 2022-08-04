cfb4a0bcedb0c6486b2e8dd942e2287ce18d5f3a
Craig Citro <craig@citro.net>
2022-08-04 07:37:47 -0500

    LSP setup

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -14,3 +14,12 @@ fi
 
 npm install -g intelephense typescript typescript-language-server
 
+mkdir -p $HOME/opt
+if [ ! -d "$HOME/opt/lua-language-server" ]; then
+    mkdir -p $HOME/opt/lua-language-server
+    cd $HOME/opt/lua-language-server
+    wget https://github.com/sumneko/lua-language-server/releases/download/3.5.2/lua-language-server-3.5.2-linux-x64.tar.gz
+    tar xzvf lua-language-server-3.5.2-linux-x64.tar.gz
+    rm -f lua-language-server-3.5.2-linux-x64.tar.gz
+fi
+
