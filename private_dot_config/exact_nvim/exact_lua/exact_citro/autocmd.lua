4360f680d3f5c2b4753a448ba87e4ba816c0e1c5
Craig Citro <craig@citro.net>
2022-08-05 08:12:56 -0500

    nvim tweaks

--- private_dot_config/exact_nvim/exact_lua/exact_citro/autocmd.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_citro/autocmd.lua
@@ -1,6 +1,9 @@
+-- automatically set the cwd to whatever dir the nvim command was launched with
 local autoCwd = vim.api.nvim_create_augroup("AutoCwd", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", {
-    command = "cd $PWD",
+    callback = function()
+        vim.api.nvim_command("lcd %:p:h")
+    end,
     group = autoCwd,
 })
 
