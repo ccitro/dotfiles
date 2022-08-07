1b28621dd9efd0dc5ef7a67fd1ed8318d1871acb
Craig Citro <craig@citro.net>
2022-08-07 19:02:21 +0000

    Add keymaps for lsp

--- private_dot_config/exact_nvim/exact_lua/exact_citro/lspconfig.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_citro/lspconfig.lua
@@ -9,7 +9,12 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
 end
 
 require 'lspconfig'.sumneko_lua.setup {
