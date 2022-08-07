9a05f63134d9d37cafb5769f0fb511e6e025cbdc
Craig Citro <craig@citro.net>
2022-08-07 18:47:25 +0000

    Treeview keymaps

--- private_dot_config/exact_nvim/exact_after/exact_plugin/nvim-tree.lua
+++ private_dot_config/exact_nvim/exact_after/exact_plugin/nvim-tree.lua
@@ -1,7 +1,9 @@
+local Remap = require("citro.keymap")
+local nnoremap = Remap.nnoremap
+
 require("nvim-web-devicons")
 require("nvim-tree").setup({
-    open_on_setup = true,
-    open_on_setup_file = true,
+    hijack_netrw = false,
     view = {
         side = "left",
     },
@@ -9,4 +11,12 @@ require("nvim-tree").setup({
         enable = true,
     },
 })
+
+-- @future make nvim-tree close itself if i quit the last open buffer
+-- this sorta works, but breaks telescope
+-- https://github.com/kyazdani42/nvim-tree.lua/issues/1368#issuecomment-1195570841
+
+nnoremap("<leader>t", function()
+    require"nvim-tree".toggle(false, true)
+end)
 
