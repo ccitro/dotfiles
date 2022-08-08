bb41224384f73c099abe23bf9dada6a9bebcf380
Craig Citro <craig@citro.net>
2022-08-08 12:22:16 -0500

    Add php lint app keybind

--- private_dot_config/exact_nvim/exact_after/exact_plugin/exact_keymap/init.lua
+++ private_dot_config/exact_nvim/exact_after/exact_plugin/exact_keymap/init.lua
@@ -3,4 +3,4 @@ local nnoremap = Remap.nnoremap
 
 nnoremap("<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 nnoremap('<leader>fu', vim.lsp.buf.formatting)
-
+nnoremap('<leader>pl', "<cmd>!cd %:h/..;~/code/app01_screens/build/lint-app.sh<CR>")
