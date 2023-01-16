4aedb133af2b97dfb74bbeaba689c32f2c28c0ec
Craig Citro <craig@citro.net>
2023-01-16 09:16:13 -0600

    Add remaps

--- private_dot_config/exact_nvim/exact_after/exact_plugin/exact_keymap/misc.lua
+++ private_dot_config/exact_nvim/exact_after/exact_plugin/exact_keymap/misc.lua
@@ -4,3 +4,11 @@ local nnoremap = Remap.nnoremap
 nnoremap('<leader>fu', vim.lsp.buf.formatting)
 nnoremap('<leader>pl', "<cmd>!cd %:h/..;~/code/app01_screens/build/lint-app.sh<CR>")
 nnoremap('<leader>pf', "<cmd>!~/code/app01_screens/build/lint-file.sh %:p --level 4<CR>")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
