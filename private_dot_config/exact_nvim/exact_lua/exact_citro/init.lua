ee1ff4a456d7cb5fb76c8c84c2660aa6a2f597fa
Craig Citro <craig@citro.net>
2023-07-13 08:36:19 -0500

    Clear out linux specific configs

--- private_dot_config/exact_nvim/exact_lua/exact_citro/init.lua
+++
@@ -1,4 +0,0 @@
-require("citro.set")
-require("citro.remap")
-require("citro.autocmd");
-require("citro.plugins");
--- run_once_vscode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-wget -O vscode.deb "https://go.microsoft.com/fwlink/?LinkID=760868" 
-sudo apt install ./vscode.deb
-rm ./vscode.deb
